75982c2312f4b2c25555c8d624d1a13cd512b72a
sidmittal32 <siddharthmittal2002@gmail.com>
2023-10-15 11:17:57 +0530

    updated ui

--- react/src/components/footer.jsx
+++ react/src/components/footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Footer() {
   return (
-      <div className="footer">
-          Made with ❤ by Cryptonite
+    <div className="footer">
+      <div>
+        Copyright © Cryptonite 2023
       </div>
+      <div>
+        Developed by <a href="https://www.linkedin.com/in/siddharth-mittal-/">Siddharth Mittal</a>
+      </div>
+    </div>
   );
 }
 
