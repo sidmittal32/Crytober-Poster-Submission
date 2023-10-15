fa8f39b42f6b34a224ec87dba1a3ee1445d605d6
sidmittal32 <siddharthmittal2002@gmail.com>
2023-10-15 14:05:08 +0530

    setup for deployment

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const base = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+
 export default defineConfig({
+  base,
   plugins: [react()],
-  server: {
-    proxy: {
-      // string shorthand
-      '/api': 'http://localhost:8080/',
-    }
-  }
-})
+  build: {
+    outDir: 'dist',
+    minify: true,
+    assetsDir: 'assets',
+    chunkFileNames: 'assets/[name].[hash].js',
+    assetFileNames: 'assets/[name].[hash].[ext]',
+    cssCodeSplit: true,
+    minify: 'terser',
+    sourcemap: true,
+  },
+});
+
