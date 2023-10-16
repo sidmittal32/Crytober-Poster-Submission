609d1eb6056edf891e1fcffa4cb5826798d467d8
sidmittal32 <siddharthmittal2002@gmail.com>
2023-10-16 18:26:46 +0530

    alert user on wrong input

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,21 +8,38 @@ export default function NewPost() {
   const [teamLeaderEmail, setTeamLeaderEmail] = useState('');
   const [teamLeaderPhone, setTeamLeaderPhone] = useState('');
   const [file, setFile] = useState('');
-  const [submissionSuccess, setSubmissionSuccess] = useState(false);
 
   const navigate = useNavigate();
 
   const validateAndSubmit = async () => {
-    if (
-      teamName.length < 3 ||
-      !/^[a-zA-Z]+$/.test(teamName) ||
-      teamLeaderName.length < 3 ||
-      !/^[a-zA-Z]+$/.test(teamLeaderName) ||
-      !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(teamLeaderEmail) ||
-      !/^\d{10}$/.test(teamLeaderPhone) ||
-      !file.type.startsWith('image/')
-    ) {
-      alert('Invalid input. Please check your entries.');
+    const errors = [];
+
+    if (teamName.length < 3 || !/^[a-zA-Z]+$/.test(teamName)) {
+      errors.push('Team Name should be at least 3 characters long and contain only letters.');
+    }
+
+    if (teamLeaderName.length < 3 || !/^[a-zA-Z]+$/.test(teamLeaderName)) {
+      errors.push('Team Leader Name should be at least 3 characters long and contain only letters.');
+    }
+
+    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(teamLeaderEmail)) {
+      errors.push('Team Leader Email is not in a valid format.');
+    }
+
+    if (!/^\d{10}$/.test(teamLeaderPhone)) {
+      errors.push('Team Leader Phone Number should be a 10-digit number.');
+    }
+
+    if (!file.type.startsWith('image/')) {
+      errors.push('Selected file is not an image. Please choose an image file.');
+    }
+
+    if (!file || !file.type.startsWith('image/')) {
+      errors.push('Please select an image file.');
+    }
+
+    if (errors.length > 0) {
+      alert('Invalid input. Please check your entries:\n\n' + errors.join('\n'));
       return;
     }
 
@@ -38,7 +55,7 @@ export default function NewPost() {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
-      setSubmissionSuccess(true);
+      navigate('/submitsuccess');
     } catch (error) {
       console.error('Error submitting the form', error);
     }
@@ -66,69 +83,61 @@ export default function NewPost() {
           <h2 className="text-center">
             <strong>Submit</strong> your poster.
           </h2>
-          {submissionSuccess ? (
-            <div className="submission-success">
-              Your Submission was Successful.
-            </div>
-          ) : (
-            <div>
-              <div className="form-group">
-                <input
-                  value={teamName}
-                  onChange={(e) => setTeamName(e.target.value)}
-                  type="text"
-                  placeholder="Team Name"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  value={teamLeaderName}
-                  onChange={(e) => setTeamLeaderName(e.target.value)}
-                  type="text"
-                  placeholder="Team Leader Name"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  value={teamLeaderEmail}
-                  onChange={(e) => setTeamLeaderEmail(e.target.value)}
-                  type="email"
-                  placeholder="Team Leader Email"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  value={teamLeaderPhone}
-                  onChange={(e) => setTeamLeaderPhone(e.target.value)}
-                  type="tel"
-                  placeholder="Team Leader Phone Number"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <input
-                onChange={fileSelected}
-                type="file"
-                accept="image/*"
-                required
-              />
-              <div className="centered-btn">
-                <button
-                  type="button"
-                  className="btn-primary"
-                  onClick={validateAndSubmit}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-          )}
+          <div className="form-group">
+            <input
+              value={teamName}
+              onChange={(e) => setTeamName(e.target.value)}
+              type="text"
+              placeholder="Team Name"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={teamLeaderName}
+              onChange={(e) => setTeamLeaderName(e.target.value)}
+              type="text"
+              placeholder="Team Leader Name"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={teamLeaderEmail}
+              onChange={(e) => setTeamLeaderEmail(e.target.value)}
+              type="email"
+              placeholder="Team Leader Email"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={teamLeaderPhone}
+              onChange={(e) => setTeamLeaderPhone(e.target.value)}
+              type="tel"
+              placeholder="Team Leader Phone Number"
+              className="form-control"
+              required
+            />
+          </div>
+          <input
+            onChange={fileSelected}
+            type="file"
+            accept="image/*"
+            required
+          />
+          <div className="centered-btn">
+            <button
+              type="button"
+              className="btn-primary"
+              onClick={validateAndSubmit}
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
