3f1552daaaa12cba7029f85ded5b6bfa78f4f185
sidmittal32 <siddharthmittal2002@gmail.com>
2023-10-16 19:27:46 +0530

    updated ui

--- react/src/components/footer.jsx
+++ react/src/components/footer.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import cryptoniteLogo from '/src/assets/cryptonite-logo.png';
+import isacLogo from '/src/assets/isac-logo.png';
 
 function Footer() {
   return (
     <div className="footer">
-      <div>
-        Copyright © Cryptonite 2023
-      </div>
-      <div>
-        Developed by <a href="https://www.linkedin.com/in/siddharth-mittal-/">Siddharth Mittal</a>
+      <div className="footer-content">
+        <div className="logos">
+          <img
+            src={cryptoniteLogo}
+            alt="Cryptonite Logo"
+            className="logo"
+          />
+          <div className="text-section">
+            <div>
+              Cryptonite 2023
+            </div>
+            <div>
+              MAHE-ISAC COE
+            </div>
+            <div>
+              Developed by <a href="https://www.linkedin.com/in/siddharth-mittal-/">Siddharth Mittal</a>
+            </div>
+          </div>
+          <img
+            src={isacLogo}
+            alt="MAHE ISAC Logo"
+            className="logo"
+          />
+        </div>
       </div>
     </div>
   );
